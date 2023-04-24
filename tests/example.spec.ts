4017e501a5d58b8383153a0c6ea262fa177072d7
Nattaphong Klinjan <nattap1901@hotmail.com>
2023-04-24 22:00:38 +0200

    Update test-case and simplefy baseUrl on test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page, baseURL }) => {
-  await page.goto(baseURL + '');
+test('Base path page title is equal to Saksbehandling', async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(baseURL!);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Saksbehandling/);
