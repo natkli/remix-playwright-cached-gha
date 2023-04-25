97cfa49f85c9dfa0bd4d4bbac2bae50510bbf316
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-04-25 07:35:17 +0200

    Update page title to match test case

--- app/routes/saksbehandling.tsx
+++ app/routes/saksbehandling.tsx
@@ -1,7 +1,7 @@
 import type { V2_MetaFunction } from '@remix-run/node';
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: 'Saksbehandling' }];
+  return [{ title: 'Remix-playwright-github-action' }];
 };
 
 export default function Index() {
